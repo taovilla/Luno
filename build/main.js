89142d024570027a783a4d52c83a3fed741d0ea1
taovilla <taovilla90@gmail.com>
2023-06-17 16:02:52 +0100

    update

--- build/main.js
+++ build/main.js
@@ -2,14 +2,36 @@ const btn = document.querySelector('.hamburger');
 const nav = document.querySelector('.hamburger-menu');
 
 btn.addEventListener('click', function(){
-    nav.classList.toggle('hidden');
-    
-    
-    
+    nav.classList.toggle('hidden');  
+     
 });
 
-//smooth scroll
 
-const scroll = new SmoothScroll('.navbar . a[href*="#"]', {
-	speed: 300
+
+//counter
+
+const counters = document.querySelectorAll('.count');
+const interval = 5000;
+
+counters.forEach((counter) =>{
+
+    let startValue = 0;
+    let endValue =+ counter.getAttribute('data-target');
+
+    let duration = Math.floor(interval / endValue);
+
+    let countStart = setInterval(function(){
+
+        startValue += 1;
+        counter.textContent = startValue;
+
+        if (startValue == endValue) {
+
+            clearInterval(countStart)
+            
+        }
+
+    } ,duration);
+
+  
 });
